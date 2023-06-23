d2692f5be12b7f9343a7fe816fe9a169cce8a165
Christophe de Dinechin <christophe@dinechin.org>
2023-06-23 23:29:28 +0200

    Add a little more details to error message when an error happens
    It is a bit difficult to locate the source position after compilation, so the
    best we can do is report the faulty command.
    
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- src/dm42/sysmenu.cc
+++ src/dm42/sysmenu.cc
@@ -476,7 +476,9 @@ static int state_load_callback(cstring path, cstring name, void *merge)
                 object::result exec = cmds->execute();
                 if (exec != object::OK)
                 {
-                    lcd_print(t24, "Error %d during loading", exec);
+                    lcd_print(t24, "Error loading file");
+                    lcd_puts(t24, (cstring) rt.error());
+                    lcd_print(t24, "executing %s", rt.command());
                     lcd_refresh();
                     wait_for_key_press();
                     return 1;
@@ -488,6 +490,7 @@ static int state_load_callback(cstring path, cstring name, void *merge)
                 utf8 ed = editor;
 
                 lcd_print(t24, "Error at byte %u", pos - ed);
+                lcd_puts(t24, (cstring) rt.error());
                 lcd_refresh();
                 beep(3300, 100);
                 wait_for_key_press();
